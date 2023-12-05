6a54ef4396594a3e2e1541b0813f5ec7e62eb784
collins401 <373345619@qq.com>
2023-12-05 11:47:40 +0800

    chore: 开发环境缓存修复

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,10 @@ export default ({ mode, command }: ConfigEnv): UserConfig => {
       port: 3456,
       open: true,
       https: false,
+      headers: {
+        // 开发环境vite或generouted偶发缓存问题，导致页面UI更新不及时
+        'cache-control': 'no-store'
+      },
       proxy: {
         '/prod-api/': {
           target: 'https://vue.ruoyi.vip',
